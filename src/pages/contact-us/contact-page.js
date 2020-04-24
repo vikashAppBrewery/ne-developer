620926feb4c61a3f340b856b42098515ad0ad086
vikashAppBrewery <vikash99318@live.com>
2020-04-24 22:36:02 +0530

    changed some more in contact page

--- src/pages/contact-us/contact-page.js
+++ src/pages/contact-us/contact-page.js
@@ -6,7 +6,8 @@ import FooterNavigation from "../home/component/footer/footer";
 import MyForm from "./component/form";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import { ContactImage } from "../../assets/contact-image-header.svg";
+import Map from "../../assets/map.svg";
+import Phone from "../../assets/phone.svg";
 
 const ContactPage = () => {
   return (
@@ -17,10 +18,35 @@ const ContactPage = () => {
         <Grid container className="contact-container">
           <Grid item xs={12} md={6} className="contact-us-content">
             <h1>Address</h1>
+            <h2>
+              <img src={Map} />
+              Main office:
+            </h2>
             <p>
-              New bazar <br />
-              Gangtok sikkim <br />
+              New bazar Gangtok sikkim <br />
               737101
+            </p>
+
+            <h2>
+              <img src={Map} />
+              Branch office:
+            </h2>
+            <p>
+              Coimbatore, Themaiyan Street <br /> Koundampalayam, Coimbatore
+              <br /> Tamil Nadu <br /> 641030
+            </p>
+            <p>
+              D-block AECS layout <br /> Kundlahalli, Marathahalli
+              <br /> Karnataka <br /> 560037
+            </p>
+            <h1>Customer Support</h1>
+            <p>
+              <img src={Phone} />
+              +91-9789939112
+            </p>
+            <p>
+              <img src={Phone} />
+              +91-8610043765
             </p>
           </Grid>
           <Grid item xs={12} md={6} className="contact-us-form">
